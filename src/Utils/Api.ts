44cd7848b4e8cdd7622f798f56ad8ea413697daa
Antonio Lindo <antoniolvitall@gmail.com>
2024-06-29 15:04:17 -0300

    Criando chamada da API

---
+++ src/Utils/Api.ts
@@ -0,0 +1,12 @@
+export const fetchApi = async () => {
+  const response = await fetch(`https://servicodados.ibge.gov.br/api/v3/noticias/?qtd=100`);
+  if (!response.ok) {
+    throw new Error('Erro');
+  }
+  try {
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
